14a1952d95142e41998be65fab8b033b0f4f0b51
Kim Gwang woon <misili304@naver.com>
2023-06-18 01:43:27 +0900

    20230618

--- week5/181921.py
+++ week5/181921.py
@@ -18,8 +18,8 @@ return 하는 solution 함수를 완성해 주세요.
 '''
 import math
 
-l = 127
-r = 1506
+l = 5
+r = 555
 
 # 0과 5로만 이루어진 수 만들기?
 # 5,
@@ -28,34 +28,47 @@ r = 1506
 # 5000, 5005, 5050, 5055, 5500, 5505, 5550, 5555
 
 
+# def solution(l, r):
+#     # 결과값을 담을 리스트
+#     result = []
+#     # for문에서 탐색 범위를 줄이기 위해 5의 배수로 만들어주기
+#     # 128이면 125이런식으로 바꿔준다 5로 나눈 몫에 5를 곱해서 가까운 5의 배수로 만들기
+#     num1 = l//5*5
+#     num2 = r//5*5 + 5
+#     # num1부터 num2 까지 5씩 체크하기
+#     for i in range(num1, num2, 5):
+#         print(set(str(i)))
+#         # 문자열도 set을 쓰면 집합으로 바꿔준다.
+#         # 0과 5 또는 5로만 이루어져 있다면 result에 추가해준다
+#         if set(str(i)) == set({"0", "5"}) or set(str(i)) == set({'5'}):
+#             result.append(i)
+#     return result if result else [-1]
+
+#     # 프로그래머스는 통과하지만 치명적인 오류가 하나 있다.
+#     # 숫자가 56이라고 하면 55를 기준으로 탐색을 해서 55을 포함시키게 된다.
+
+
+
+
+# def solution(l, r):
+#     result = []
+#     num1 = l//5*5
+#     num2 = r//5*5 + 5
+#     for i in range(num1, num2, 5):
+#         if set(str(i)) == set({"0", "5"}) or set(str(i)) == set({'5'}):
+#             result.append(i)
+#     return result if result else [-1]
+
+
+# from itertools import permutations
+
+def generate_numbers(a, b, n):
+    digits = [str(a), str(b)]
+    numbers = digits[:]
+    for _ in range(n-1):
+        numbers = [x + y for x in numbers for y in digits]
+    return {int(num) for num in numbers}
+
 def solution(l, r):
-    # 결과값을 담을 리스트
-    result = []
-    # for문에서 탐색 범위를 줄이기 위해 5의 배수로 만들어주기
-    # 128이면 125이런식으로 바꿔준다 5로 나눈 몫에 5를 곱해서 가까운 5의 배수로 만들기
-    num1 = l//5*5
-    num2 = r//5*5 + 5
-    # num1부터 num2 까지 5씩 체크하기
-    for i in range(num1, num2, 5):
-        print(set(str(i)))
-        # 문자열도 set을 쓰면 집합으로 바꿔준다.
-        # 0과 5 또는 5로만 이루어져 있다면 result에 추가해준다
-        if set(str(i)) == set({"0", "5"}) or set(str(i)) == set({'5'}):
-            result.append(i)
-    return result if result else [-1]
-
-    # 프로그래머스는 통과하지만 치명적인 오류가 하나 있다.
-    # 숫자가 56이라고 하면 55를 기준으로 탐색을 해서 55을 포함시키게 된다.
-
-
-print(solution(l, r))
-
-
-def solution(l, r):
-    result = []
-    num1 = l//5*5
-    num2 = r//5*5 + 5
-    for i in range(num1, num2, 5):
-        if set(str(i)) == set({"0", "5"}) or set(str(i)) == set({'5'}):
-            result.append(i)
-    return result if result else [-1]
+    result = generate_numbers(0, 5, len(str(r))) & {i for i in range(l, r+1)}
+    return sorted(list(result)) if result else [-1]
